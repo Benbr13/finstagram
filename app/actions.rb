7df72d43e8905bcd940fa9ef220db50af988bae5
bbrouillette <bbrouillette@labplas.com>
2019-10-10 20:16:57 -0400

    version8

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,14 @@
 get '/' do
   File.read(File.join('app/views','index.html'))
 end
+
+#when a browser request the root of the application
+get '/' do
+  
+  # Send the string "hello the world" to the browser
+    "hello world!"
+  
+  # Stop
+end
+
+
